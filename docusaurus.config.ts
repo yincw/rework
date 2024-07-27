bce194b9d6ed7e50ac0fdfcd319e8e14aec55308
yincw <yincw@vanke.com>
2024-07-27 17:06:56 +0800

    add

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
   url: 'https://yincw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  // baseUrl: '/rework/',
-  baseUrl: '/',
+  baseUrl: '/rework/',
+  // baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
